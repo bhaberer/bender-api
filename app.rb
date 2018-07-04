feb43b814c2f02b9ddaacb971c7c0b215442aac9
Brian Haberer <bhaberer@gmail.com>
2018-07-03 19:16:30 -0700

    Skeleton

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'grape'
+require_relative 'plugins/countdowns'
+
+module Bender
+  class API < Grape::API
+    format :json
+    prefix :api
+    mount Countdowns
+
+    desc 'Countdown till Pax'
+    get '/' do
+      'Foo'
+    end
+  end
+end
