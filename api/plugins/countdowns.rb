23ebe2e75dcce2dee37d749ccc6baaa03d3929a3
Brian Haberer <bhaberer@gmail.com>
2018-07-03 19:25:32 -0700

    Skeleton

--- api/plugins/countdowns.rb
+++ api/plugins/countdowns.rb
@@ -1,8 +1,11 @@
 class Countdowns < Grape::API
   format :json
 
-  desc 'Countdown till Pax'
-  get :pax do
-    { text: 'So many days' }
+  resource :countdowns do
+    desc 'Countdown till Pax'
+    get :pax do
+      { text: 'So many days' }
+    end
   end
+
 end
