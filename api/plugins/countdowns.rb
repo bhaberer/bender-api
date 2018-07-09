09252c7328c635bf976adee37ba3941f8e56a6c7
Brian Haberer <bhaberer@gmail.com>
2018-07-09 14:56:57 -0700

    asdsd

--- api/plugins/countdowns.rb
+++ api/plugins/countdowns.rb
@@ -18,7 +18,7 @@ module Plugin
       build_paxes.each do |pax|
         desc "Countdown till #{pax.pax_type}"
         get "pax/#{pax.pax_type}" do
-          next_pax = get_next_pax(type: pax.pax_type)
+          next_pax = get_next_pax(pax_type: pax.pax_type)
           { text: next_pax.time_till }
         end
       end
